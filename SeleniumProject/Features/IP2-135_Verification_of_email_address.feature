f50e090c70aaa2a1e7c7753262b6a8c5f4d121a1
Katarzyna Kochanek <katkochanek@gmail.com>
2021-04-02 11:39:46 +0200

    create GWT Test for email address verification

---
+++ SeleniumProject/Features/IP2-135_Verification_of_email_address.feature
@@ -0,0 +1,33 @@
+﻿Feature: Verification of email address
+User should be able to confirm his/hers email address with received code.
+	
+
+Scenario: User inserts correct code
+	Given User is on registration site
+	And is in possession of code 
+	When User is entering the code
+	Then Button "Zatwierdź kod" becomes active, can be clicked
+	And redirects User on another page
+
+
+Scenario: User should be able to retrive code
+	Given User is on registration site
+	And doesn't have code
+	When User clicks "Nie otrzymałem/am kodu"
+	Then User should receive code one more time
+	
+
+Scenario: User shouldn't be able to register with invalid code
+	Given User is on registration site
+	When User enters false code
+	Then User should be moved on the site, where "Wystąpił błąd" is visible.
+
+Scenario: User shouldn't be able to click the button, which has less than 8 characters
+	Given User is on registration site
+	When User enters incorrect code
+	Then Button "Zatwierdz kod" should remain inactive
+
+Scenario: User shouldn't be able to click the button, which has more than 8 characters
+	Given User is on registration site
+	When User enters incorrect code
+	Then Button "Zatwierdz kod" should remain inactive
---
+++ SeleniumProject/Steps/VerificationOfEmailAdressSteps.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumProject.Steps
+{
+    [Binding]
+    public class VerificationOfEmailAddressSteps
+    {
+        [Given(@"User is on registration site")]
+        public void GivenUserIsOnRegistrationSite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"is in possession of code")]
+        public void GivenIsInPossessionOfCode()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"doesn't have code")]
+        public void GivenDoesnTHaveCode()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User is entering the code")]
+        public void WhenUserIsEnteringTheCode()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User clicks ""(.*)""")]
+        public void WhenUserClicks(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User enters false code")]
+        public void WhenUserEntersFalseCode()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"User enters incorrect code")]
+        public void WhenUserEntersIncorrectCode()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Button ""(.*)"" becomes active, can be clicked")]
+        public void ThenButtonBecomesActiveCanBeClicked(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"redirects User on another page")]
+        public void ThenRedirectsUserOnAnotherPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User should receive code one more time")]
+        public void ThenUserShouldReceiveCodeOneMoreTime()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User should be moved on the site, where ""(.*)"" is visible\.")]
+        public void ThenUserShouldBeMovedOnTheSiteWhereIsVisible_(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Button ""(.*)"" should remain inactive")]
+        public void ThenButtonShouldRemainInactive(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
