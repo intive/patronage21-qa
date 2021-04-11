a261cdcab2cdf6fcacd3fd80962c6cf04e9ad4da
AdamAndrysek <adam.andrysek@icloud.com>
2021-04-11 02:11:23 +0200

    Added feature file and step definitions for API user search

---
+++ RestSharpProject/Features/UserSearch.feature
@@ -0,0 +1,42 @@
+﻿Feature: User Search
+	User Story: There will be a user story when it's ready
+	Task in Java Board: https://tracker.intive.com/jira/browse/IP2-90
+	Task in QA Board: https://tracker.intive.com/jira/browse/IP2-193
+
+Background: 
+Given URL endpoint is /api/users
+
+@mytag
+# link do testu 1 w Zephyr
+Scenario Outline: 1_Searching for the existing user by name, surname or username
+Given Customer sets the endpoint with method GET
+And Customer enters <searchData> as a query parameter
+And <searchData> exists in the database
+When Customer sends the request to the endpoint
+Then The server returns code 200 
+And JSON body contains <searchData>
+Examples:
+| searchData |
+| Imie       |
+| Nazwisko   |
+| Username   |
+
+# link do testu 2 w Zephyr
+Scenario Outline: 2_Searching for the non-existing user
+Given Customer sets the endpoint with method GET
+And Customer enters <searchData> as a query parameter
+And <searchData> doesn't exist in the database
+When Customer sends the request to the endpoint
+Then The server returns code 400 
+And JSON body contains message "Użytkownik nie istnieje"
+Examples:
+| searchData       |
+| NieznaneImie     |
+| NieznaneNazwisko |
+| NieznanyUsername |
+
+# link do testu 3 w Zephyr
+Scenario: 3_Using non-allowed methods for the endpoint
+Given Customer sets the endpoint with non-allowed method
+When Customer sends the request to the endpoint
+Then The server returns code 405
---
+++ RestSharpProject/Steps/UserSearchSteps.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace RestSharpProject.Steps
+{
+    [Binding]
+    public class UserSearchSteps
+    {
+        [Given(@"URL endpoint is /api/users")]
+        public void GivenURLEndpointIsApiUsers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Customer sets the endpoint with method GET")]
+        public void GivenCustomerSetsTheEndpointWithMethodGET()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Customer enters Imie as a query parameter")]
+        public void GivenCustomerEntersImieAsAQueryParameter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Imie exists in the database")]
+        public void GivenImieExistsInTheDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Customer enters NieznaneImie as a query parameter")]
+        public void GivenCustomerEntersNieznaneImieAsAQueryParameter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"NieznaneImie doesn't exist in the database")]
+        public void GivenNieznaneImieDoesnTExistInTheDatabase()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Customer sets the endpoint with non-allowed method")]
+        public void GivenCustomerSetsTheEndpointWithNon_AllowedMethod()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Customer sends the request to the endpoint")]
+        public void WhenCustomerSendsTheRequestToTheEndpoint()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The server returns code (.\d+)")]
+        public void ThenTheServerReturnsCode(int responseCode)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"JSON body contains Imie")]
+        public void ThenJSONBodyContainsImie()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"JSON body contains message ""(.+)""")]
+        public void ThenJSONBodyContainsMessage(string errorMessage)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
