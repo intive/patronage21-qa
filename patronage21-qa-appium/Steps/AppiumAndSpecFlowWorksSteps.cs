6fc00d07b81323755b66eed6bc431c61421cc163
Paulina Dzinowagis <paulinadzinowagis@gmail.com>
2021-04-15 20:57:20 +0200

    [IP2-171] create BaseSteps class to move common part of each test

--- patronage21-qa-appium/Steps/AppiumAndSpecFlowWorksSteps.cs
+++ patronage21-qa-appium/Steps/AppiumAndSpecFlowWorksSteps.cs
@@ -1,40 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Threading;
+﻿using System.Threading;
 using NUnit.Framework;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
 using TechTalk.SpecFlow;
 
 namespace patronage21_qa_appium.Steps
 {
     [Binding]
-    public class AppiumAndSpecFlowWorksSteps
+    public class AppiumAndSpecFlowWorksSteps : BaseSteps
     {
-        private AppiumDriver<AndroidElement> _driver;
-
-        [Before]
-        public void Setup()
-        {
-            var appFileName = "Patron-a-tive.apk";
-            var projectBaseDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            var appPath = Path.Combine(projectBaseDir, @"apps/", appFileName);
-
-            var driverOption = new AppiumOptions();
-            driverOption.AddAdditionalCapability("platformName", "Android");
-            driverOption.AddAdditionalCapability("deviceName", "Android 30");
-            driverOption.AddAdditionalCapability("app", appPath);
-
-            _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
-        }
-
-        [AfterScenario]
-        public void TearUp()
-        {
-            _driver.CloseApp();
-        }
-
         [Given(@"I run app using Appium server")]
         public void GivenIRunAppUsingAppiumServer()
         {
--- patronage21-qa-appium/Steps/HomeNavigationSteps.cs
+++ patronage21-qa-appium/Steps/HomeNavigationSteps.cs
@@ -1,38 +1,11 @@
 ﻿using NUnit.Framework;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Android;
-using System;
-using System.IO;
 using TechTalk.SpecFlow;
 
 namespace patronage21_qa_appium.Steps
 {
     [Binding]
-    public class HomeNavigationSteps
+    public class HomeNavigationSteps : BaseSteps
     {
-        private AppiumDriver<AndroidElement> _driver;
-
-        [Before]
-        public void Setup()
-        {
-            var appFileName = "Patron-a-tive.apk";
-            var projectBaseDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-            var appPath = Path.Combine(projectBaseDir, @"apps/", appFileName);
-
-            var driverOption = new AppiumOptions();
-            driverOption.AddAdditionalCapability("platformName", "Android");
-            driverOption.AddAdditionalCapability("deviceName", "Android 30");
-            driverOption.AddAdditionalCapability("app", appPath);
-
-            _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
-        }
-
-        [AfterScenario]
-        public void TearUp()
-        {
-            _driver.CloseApp();
-        }
-
         [Given(@"I am on Home page")]
         public void GivenIAmOnHomePage()
         {
