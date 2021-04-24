b35902c5c01255e9aab07a39b3070fd7ce419b71
Patrycja Hinca <patrycja.hinca19@gmail.com>
2021-04-24 12:12:16 +0200

    added new changes

---
+++ SeleniumProject/Features/SuccessSide.feature
@@ -0,0 +1,7 @@
+﻿Feature: SuccessSide
+
+#link to Zephyr_1 test
+Scenario: SUCCESS_SIDE_1_IP2-175_The_user_makes_a_successful_registration_request
+	Given the user sees the registration success message
+	When the user clicks on button <zamknij>
+	Then the user should be redirected to their page <Witaj w Patron-a-tive!>
---
+++ SeleniumProject/Features/SuccessSideSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumProject.Features
+{
+    [Binding]
+    public class SuccessSideSteps
+    {
+        [Given(@"the user sees the registration success message")]
+        public void GivenTheUserSeesTheRegistrationSuccessMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on button (.*)")]
+        public void WhenTheUserClicksOnButton(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the user should be redirected to their page (.*)")]
+        public void ThenTheUserShouldBeRedirectedToTheirPage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
