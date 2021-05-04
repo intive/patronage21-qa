a8b05d7f64ac1c139140fe998c99d28bdea91628
katkoch <78875093+katkoch@users.noreply.github.com>
2021-05-04 21:16:50 +0200

    feature/BDD-CODE-IP2-288-Success-Site-Confirmation-Of-Participation (#29)
    * new version of IP2-288
    * changes according to review
    * fixed mistake
    * Zephyr link added

---
+++ SeleniumProject/Hooks/Hooks.cs
@@ -0,0 +1,46 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        public static IWebDriver driver;
+        public static IObjectContainer _objectContainer;
+
+        public Hooks(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            options.AddArgument("--start-maximized");
+
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "http://localhost:3000";
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _objectContainer.RegisterInstanceAs<IWebDriver>(driver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
+
