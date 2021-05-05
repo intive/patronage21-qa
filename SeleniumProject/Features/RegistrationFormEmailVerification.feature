43058f9c1ad1d3063b76a04baad5156dedf5c95d
Katarzyna Kochanek <katkochanek@gmail.com>
2021-05-05 19:50:48 +0200

    code with PageObject

--- SeleniumProject/Features/RegistrationFormEmailVerification.feature
+++ SeleniumProject/Features/RegistrationFormEmailVerification.feature
@@ -26,13 +26,13 @@ Scenario: REGISTRATION_FORM_2_IP2-135_User_should_be_able_to_retrieve_code_on_ma
 
 #https://tracker.intive.com/jira/browse/IP2-299
 Scenario: REGISTRATION_FORM_3_IP2-135_False_code_should_not_allow_User_to_be_verified_and_activated
-	When User enters code 00000000
+	When User enters code '00000000'
 	And User clicks 'Zatwierdź kod' button
 	Then User sees 'Błędny kod'
 
 #https://tracker.intive.com/jira/browse/IP2-300
 Scenario: REGISTRATION_FORM_4_IP2-135_Too_short_code_should_not_allow_User_to_be_verified_and_activated 
-	When User enters code 1234
+	When User enters code '1234'
 	Then User sees 'Wprowadzony kod jest zbyt krótki'
 	And is not able to click 'Zatwierdź kod' button
 	
@@ -56,20 +56,23 @@ Scenario: REGISTRATION_FORM_6_IP2-135_Generated_code_is_unique
 
 #https://tracker.intive.com/jira/browse/IP2-518
 Scenario: REGISTRATION_FORM_7_IP2-135_Retrieving_of_the_code_should_be_possible
+	Given User enters data
+	| firstName | lastName | email             | phone       | githubLink             | login        | password         | passwordConfirm  |
+	| Jan       | Nowak    | example@email.com | 123456789   | github.com/exampleLink | exampleLogin | examplePassword@ | examplePassword@ |
+	And is transfered to verifications site
 	When User clicks 'Nie otrzymałem/am kodu'
-	Then User should see message about sending new code
-	
+	Then User sees 'Kod aktywacyjny został wysłany pomyśnie'
 
 #https://tracker.intive.com/jira/browse/IP2-519
 Scenario: REGISTRATION_FORM_8_IP2-135_Improper_code_should_not_allow_User_to_be_verified_and_activated 
 	When User enters code '    1234'
 	And User clicks 'Zatwierdź kod' button
-	Then User sees 'Wprowadzony kod jest niepoprawny'
+	Then User sees 'Błędny kod'
 
 #https://tracker.intive.com/jira/browse/IP2-520
 Scenario: REGISTRATION_FORM_9_IP2-135_Only_numbers_can_be_inserted_as_code
-	When User enters <code_with_characters> 
-	| code_with_characters |
+	When User enters <code with characters> 
+	| code with characters |
 	| 'aaaaaa'             |
 	| '123a455'            |
 	Then is not able to click 'Zatwierdź kod' button
---
+++ SeleniumProject/Steps/VerificationOfEmailAddressSteps.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumProject.Pages;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SeleniumProject.Steps
+{
+    [Binding]
+    [Scope(Feature = "Verification of email address")]
+    public class VerificationOfEmailAddressSteps
+    {
+        private readonly IWebDriver _webdriver;
+        private readonly VerificationOfEmailAddressPage verificationOfEmailAddressPage;
+
+        public VerificationOfEmailAddressSteps(IWebDriver driver)
+        {
+            _webdriver = driver;
+            verificationOfEmailAddressPage = new VerificationOfEmailAddressPage(_webdriver);
+        }
+
+        [Given(@"User proceeds with registration via email verification page")]
+        public void GivenUserProceedsWithRegistrationViaEmailVerificationPage()
+        {
+            _webdriver.Url = _webdriver.Url + "weryfikacja";
+        }
+
+        [Given(@"User enters data")]
+        public void GivenUserEntersData(Table table)
+        {
+            _webdriver.Url = "http://localhost:3000/rejestracja";
+
+            dynamic data = table.CreateDynamicInstance();
+
+            verificationOfEmailAddressPage.firstName.SendKeys(data.firstName);
+            verificationOfEmailAddressPage.lastName.SendKeys(data.lastName);
+            verificationOfEmailAddressPage.email.SendKeys(VerificationOfEmailAddressPage.GenerateEmailAdress());
+            verificationOfEmailAddressPage.phone.SendKeys(data.phone.ToString());
+            verificationOfEmailAddressPage.githubLink.SendKeys(VerificationOfEmailAddressPage.GenerateGithubLink());
+            verificationOfEmailAddressPage.login.SendKeys(VerificationOfEmailAddressPage.GenerateLogin());
+            verificationOfEmailAddressPage.password.SendKeys(data.password);
+            verificationOfEmailAddressPage.passwordConfirm.SendKeys(data.passwordConfirm);
+            verificationOfEmailAddressPage.checkBoxQA.Click();
+            verificationOfEmailAddressPage.checkBoxTermsAndConditions.Click();
+        }
+
+        [Given(@"is transfered to verifications site")]
+        public void GivenIsTransferedToVerificationsSite()
+        {
+            verificationOfEmailAddressPage.zalozKontoButton.Click();
+        }
+
+        [When(@"User enters code '(.*)'")]
+        public void WhenUserEntersCode(string p0)
+        {
+            verificationOfEmailAddressPage.codeInput.SendKeys(p0);
+        }
+
+        [When(@"User clicks '(.*)' button")]
+        public void WhenUserClicksButton(string p0)
+        {
+            verificationOfEmailAddressPage.confirmationButton.Click();
+        }
+
+        [When(@"User clicks '(.*)'")]
+        public void WhenUserClicks(string p0)
+        {
+            verificationOfEmailAddressPage.renewalSentButton.Click();
+        }
+
+        [When(@"User enters (.*)")]
+        public void WhenUserEnters(string p0, Table table)
+        {
+            verificationOfEmailAddressPage.codeInput.SendKeys(p0);
+        }
+
+        [Then(@"User sees '(.*)'")]
+        public void ThenUserSees(string p0)
+        {
+            Assert.AreEqual(true, verificationOfEmailAddressPage.getErrorMessage(p0).Displayed);
+        }
+
+        [Then(@"is not able to click '(.*)' button")]
+        public void ThenIsNotAbleToClickButton(string p0)
+        {
+            Assert.That(verificationOfEmailAddressPage.confirmationButton.Enabled, Is.False);
+        }
+    }
+}
