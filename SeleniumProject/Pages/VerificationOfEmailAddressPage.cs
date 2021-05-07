8dd9b1f01803cf9d8d90140b5b0c16bf4eef32cf
Katarzyna Kochanek <katkochanek@gmail.com>
2021-05-07 13:05:53 +0200

    trash

--- SeleniumProject/Pages/VerificationOfEmailAddressPage.cs
+++ SeleniumProject/Pages/VerificationOfEmailAddressPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace SeleniumProject.Pages
 {
     class VerificationOfEmailAddressPage
     {
+        IWebDriver driver;
+        public VerificationOfEmailAddressPage (IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public IWebElement 
     }
 }
