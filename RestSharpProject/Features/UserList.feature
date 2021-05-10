20e437134a0147a384eed4352b3ad8b89369dfe9
Katarzyna Kochanek <katkochanek@gmail.com>
2021-05-10 21:45:33 +0200

    new feature

---
+++ RestSharpProject/Features/UserList.feature
@@ -0,0 +1,21 @@
+﻿Feature: User list
+This feature will allow to show a list of users, which are activated.
+
+# link JS: https://tracker.intive.com/jira/browse/IP2-294
+# link QA: https://tracker.intive.com/jira/browse/IP2-312
+
+Background:
+Given Endpoint is api/list
+
+#Zephyr
+Scenario: USER_LIST_1_IP2-294_List_of_all_user_is_visible
+When Client send request with active parameter set to false
+Then Response is successfull
+And contains list of all users
+
+#Zephyr
+Scenario: USER_LIST_2_IP2-294_ List_of_active_users_is_visible
+When Client send request with active parameter set to true
+Then Response is successfull
+And contains list of active users
+
---
+++ RestSharpProject/Steps/UserListSteps.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using TechTalk.SpecFlow;
+
+namespace RestSharpProject.Steps
+{
+    [Binding]
+    [Scope(Feature = "User list")]
+    public class UserListSteps
+    {
+        private IRestClient restClient = new RestClient();
+        private IRestRequest restRequestUserList;
+        private IRestResponse restResponseList = new RestResponse();
+        private string url;
+
+        [Given(@"Endpoint is api/list")]
+        public void GivenEndpointIsApiList()
+        {
+            url = "http://127.0.0.1:8080/api/list";
+        }
+
+        [When(@"Client send request with active parameter set to false")]
+        public void WhenClientSendRequestWithActiveParameterSetToFalse()
+        {
+            restRequestUserList = new RestRequest(url + "?active=false");
+            restResponseList = restClient.Get(restRequestUserList);
+        }
+
+        [When(@"Client send request with active parameter set to true")]
+        public void WhenClientSendRequestWithActiveParameterSetToTrue()
+        {
+            restRequestUserList = new RestRequest(url + "?active=true");
+            restResponseList = restClient.Get(restRequestUserList);
+        }
+
+        [Then(@"Response is successfull")]
+        public void ThenResponseIsSuccessfull()
+        {
+            Assert.That(restResponseList.IsSuccessful, Is.True);
+        }
+
+        [Then(@"contains list of all users")]
+        public void ThenContainsListOfAllUsers()
+        {
+            Assert.That(restResponseList.Content.Contains("\"active\":false"), Is.True);
+            Assert.That(restResponseList.Content.Contains("\"active\":true"), Is.True);
+        }
+
+        [Then(@"contains list of active users")]
+        public void ThenContainsListOfActiveUsers()
+        {
+            Assert.That(restResponseList.Content.Contains("\"active\":true"), Is.True);
+        }
+    }
+}
