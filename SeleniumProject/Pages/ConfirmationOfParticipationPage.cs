65d1a99edb10f2b3ecc03cf7e44b238f8b7bf3a0
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2021-05-18 13:45:15 +0200

    Fix

---
+++ SeleniumProject/Pages/ConfirmationOfParticipationPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumProject.Pages
+{
+    class ConfirmationOfParticipationPage
+    {
+        IWebDriver driver;
+        public ConfirmationOfParticipationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement successfulRegistrationText => driver.FindElement(By.XPath("//*[text()[contains(.,'Twoja rejestracja przebiegła pomyślnie!')]]"));
+        public IWebElement mainPageButton => driver.FindElement(By.XPath("//*[text()[contains(.,'Strona główna')]]"));
+        // TO DO: change xpath when site will be updated
+        public By mainPageContentElement = By.XPath("//div[text()='tu kontent']");
+        public IWebElement mainPageContent => driver.FindElement(mainPageContentElement);
+    }
+}
