a9d46e460a3951825a7f02af2b4f5e7e014c8b28
Marta9pa <marta-paluszek@wp.pl>
2021-05-19 00:12:48 +0200

    feature/BDD_CODE_IP2-525_List_of_projects

---
+++ RestSharpProject/Steps/ListOfProjectsSteps.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestSharpProject.Models;
+using TechTalk.SpecFlow;
+
+namespace RestSharpProject.Steps
+{
+    [Binding]
+    public class ListOfProjectsSteps
+    {
+        RestClient client = new RestClient();
+        IRestRequest request = new RestRequest();
+
+
+        [Given(@"User sets the url")]
+        public void GivenUserSetsTheUrl()
+        {
+            client = new RestClient("http://intive-patronage.pl:9101");
+        }
+
+        [When(@"User sends a GET request")]
+        public void WhenUserSendsAGETRequest()
+        {
+            request = new RestRequest("/api/projects?year=2021", Method.GET);
+        }
+
+        [Then(@"Server returns the code (.*) and JSON body contain a list of projects")]
+        public void ThenServerReturnsTheCodeAndJSONBodyContainAListOfProjects(int code)
+        {
+            IRestResponse response = client.Execute(request);
+            var responseMessage = JsonConvert.DeserializeObject<Project>(response.Content);
+
+            Assert.AreEqual(code, (int)response.StatusCode);
+            Assert.That(responseMessage.projects[0], Is.EqualTo("Projekt I"));
+            Assert.That(responseMessage.projects[1], Is.EqualTo("Projekt II"));
+            Assert.That(responseMessage.projects[2], Is.EqualTo("Projekt III"));
+        }
+    }
+}
