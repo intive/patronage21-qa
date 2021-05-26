5262584cdaef220a0ead019b294983084199324f
unknown <szymonjarzab1997@gmail.com>
2021-05-26 08:34:23 +0200

    IP2-474 page object model refactor

--- patronage21-qa-appium/Steps/UsersScreenTechGroupsFromApiSteps.cs
+++ patronage21-qa-appium/Steps/UsersScreenTechGroupsFromApiSteps.cs
@@ -6,16 +6,27 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using RestSharp;
 using TechTalk.SpecFlow;
+using patronage21_qa_appium.Screens;
+using patronage21_qa_appium.Drivers;
 
 namespace patronage21_qa_appium.Steps
 {
     [Binding]
+    [Scope(Feature = "UsersScreenTechGroupsFromApi")]
     public class UsersScreenTechGroupsFromApiSteps
     {
         private string _url;
         private RestClient _client;
         private RestRequest _requestGet;
         private List<string> _groups;
+
+        private LoginScreen _loginScreen;
+        private RegisterScreen _registerScreen;
+        private ActivationScreen _activationScreen;
+        private RegisterSubmitScreen _registerSubmitScreen;
+        private HomeScreen _homeScreen;
+        private UsersScreen _usersScreen;
+        private JavaDatabase _javaDatabase;
 
         private readonly AppiumDriver<AndroidElement> _driver;
 
@@ -32,26 +43,33 @@ namespace patronage21_qa_appium.Steps
         [Given(@"User is on ""(.*)"" screen")]
         public void GivenUserIsOnScreen(string p0)
         {
-            _driver.FindElementByXPath("//android.widget.ImageView[@content-desc='Miniaturka modułu użytkowników']").Click();
+            _loginScreen.ClickElement(_driver, "Rejestracja");
+            _registerScreen.Wait(_driver);
+            _registerScreen.SubmitRegisterForm(_driver, "Pan", "test", "Nazwisko", "test@email.com", "123456789",
+                true, false, false, false, p0, "TechGroups1!", "TechGroups1!", "", true, true, true);
+            _activationScreen.Wait(_driver);
+            // to be changed, there is no code table in database yet
+            // string code = _javaDatabase.GetProperty("code", "patronative.code_user", "user", p0);
+            string code = "99999999";
+            _activationScreen.WriteTextToField(_driver, code, "Kod");
+            _activationScreen.ClickElement(_driver, "Zatwierdź kod");
+            _registerSubmitScreen.Wait(_driver);
+            _registerSubmitScreen.ClickElement(_driver, "Zamknij");
         }
 
         [When(@"User clicks ""(.*)""")]
         public void WhenUserClicks(string p0)
         {
-            var elementXpath = "//android.widget.FrameLayout/android.view.ViewGroup/android.view.View/android.view.View[3]";
-            var element = _driver.FindElementByXPath(elementXpath);
-            element.Click();
+            _usersScreen.ClickElement(_driver, "Wybierz grupę");
         }
 
         [Then(@"User sees correct tech groups")]
         public void ThenUserSeesCorrectTechGroups()
         {
-            var displayedGroupsXpath = "//android.widget.ScrollView/android.view.View";
-            var displayedGroups = _driver.FindElementsByXPath(displayedGroupsXpath);
+            var displayedGroups = _usersScreen.GetElements(_driver, "Grupy");
             for (var i = 0; i < _groups.Count; i++)
             {
-                var elementXpath = "//android.view.View[@text='" + _groups[i] + "']";
-                var element = _driver.FindElementsByXPath(elementXpath);
+                var element = _usersScreen.GetElements(_driver, _groups[i]);
                 Assert.AreEqual(1, element.Count);
             }
             Assert.AreEqual(_groups.Count, displayedGroups.Count);
