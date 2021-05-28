0e54bc966e785f89dc4b8ae8178ddc71f494271a
Szymon Jarząb <56318458+js40598@users.noreply.github.com>
2021-05-28 14:20:03 +0200

    feature/IP2-265-BDD-CODE-deactivate-profile (#56)
    * IP2-265 scenarios implemented
    
    * IP2-265 scenarios
    
    * IP2-265 page object model and database connection
    
    * IP2-265 java database execute sql command
    
    * IP2-265 pob development
    
    * IP2-265 reformat
    
    * IP2-265 reformat
    
    * IP2-265 reformat
    
    * IP2-265 zephyr links commented out

---
+++ patronage21-qa-appium/Screens/LoginScreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace patronage21_qa_appium.Screens
+{
+    internal class LoginScreen : BaseScreen
+    {
+        private static readonly string _screenName = "Logowanie";
+        public static Dictionary<string, string> _screenXpath = _screensXpathDict[_screenName];
+
+        public void ClickElement(AppiumDriver<AndroidElement> driver, string elementName)
+        {
+            base.ClickElement(driver, _screenName, elementName);
+        }
+
+        public void WriteTextToField(AppiumDriver<AndroidElement> driver, string text, string field)
+        {
+            base.WriteTextToField(driver, _screenName, text, field);
+        }
+
+        public AndroidElement GetElement(AppiumDriver<AndroidElement> driver, string elementName)
+        {
+            return base.GetElement(driver, _screenName, elementName);
+        }
+
+        public IReadOnlyCollection<AndroidElement> GetElements(AppiumDriver<AndroidElement> driver, string elementName)
+        {
+            return base.GetElements(driver, _screenName, elementName);
+        }
+
+        public static void SubmitLoginForm(AppiumDriver<AndroidElement> driver, string login, string password)
+        {
+            driver.FindElementByXPath(_screenXpath["Login"]).SendKeys(login);
+            driver.FindElementByXPath(_screenXpath["Hasło"]).SendKeys(password);
+            driver.FindElementByXPath(_screenXpath["Zaloguj"]).Click();
+        }
+    }
+}
