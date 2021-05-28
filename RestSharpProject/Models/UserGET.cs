b0b1a71a07ba30230f25ddd6830150cfa4086512
Marta9pa <marta-paluszek@wp.pl>
2021-05-29 00:17:25 +0200

    BDD_CODE_IP2-194 & 189 & 485

---
+++ RestSharpProject/Models/UserGET.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpProject.Models
+{
+
+    public class UserGET
+    {
+        public string login { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<string> image { get; set; } = null!;
+        public string status { get; set; }
+    }
+
+    public class Root
+    {
+        public List<UserGET> users { get; set; }
+    }
+
+    public class RejectedValue
+    {
+
+    }
+
+    public class ViolationError
+    {
+        public string fieldName { get; set; }
+        public string message { get; set; }
+    }
+
+    public class RootResponse
+    {
+        public List<ViolationError> violationErrors { get; set; }
+    }
+}
+
+
+
+
+
+
