41735dd34d52e44e5640baa7d3532cca3b6ac8e8
Katarzyna Kochanek <katkochanek@gmail.com>
2021-06-05 11:11:09 +0200

    update of BaseSteps

--- SeleniumProject/Steps/RegistrationSuccessSiteSteps.cs
+++ SeleniumProject/Steps/RegistrationSuccessSiteSteps.cs
@@ -1,10 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 using SeleniumProject.Pages;
-using System;
-using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace SeleniumProject.Steps
@@ -14,12 +10,11 @@ namespace SeleniumProject.Steps
     {
         private readonly IWebDriver _webdriver;
         private readonly RegistrationSuccessSitePage registrationSuccessSitePage;
-        private readonly BasePage basePage;
 
         public RegistrationSuccessSiteSteps(IWebDriver driver)
         {
             _webdriver = driver;
-            registrationSuccessSitePage = new RegistrationSuccessSitePage(_webdriver);            
+            registrationSuccessSitePage = new RegistrationSuccessSitePage(_webdriver);
         }
 
         [Given(@"Activated User is redirected to Success Site")]
@@ -29,21 +24,10 @@ namespace SeleniumProject.Steps
         }
 
         [Given(@"User sees the registration success message on site")]
-
         public void GivenUserUserSeesTheRegistrationSuccessMessage()
 
         {
             Assert.AreEqual(true, registrationSuccessSitePage.successfulRegistrationText.Displayed);
-        }
-
-        [Then(@"User should be transferred to main site")]
-        public void ThenUserShouldBeTransferredToMainSite()
-        {
-            var wait = new WebDriverWait(_webdriver, new TimeSpan(0, 0, 10));
-                      
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(basePage.mainPageContentElement));
-
-            Assert.AreEqual(true, basePage.mainPageContent.Displayed);
         }
     }
 }
