1b73a9dfd10d0153d2576152db81a6525872932a
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2021-06-10 18:03:52 +0200

    Created navigation page

---
+++ SeleniumProject/Pages/NavigationPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Pages
+{
+    public class NavigationPage
+    {
+        private IWebDriver driver;
+
+        public NavigationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private By users = By.XPath("//*[text()='Użytkownicy']//parent::button");
+        private By calendar = By.XPath("//*[text()='Kalendarz']//parent::button");
+
+        private IWebElement usersModule => driver.FindElement(users);
+        private IWebElement calendarModule => driver.FindElement(calendar);
+
+        public void NavigateToCalendarSite() => calendarModule.Click();
+        public void NavigateToUsersSite() => usersModule.Click();
+    }
+}
