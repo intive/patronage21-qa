5249aaef0e6c329d272eea681e264b8e2d8d7e57
Marcin Kmiecik <sggp.kmiecik@gmail.com>
2021-06-11 11:34:47 +0200

    Fixed steps for new hooks

--- RestSharpJS/Steps/UserListSteps.cs
+++ RestSharpJS/Steps/UserListSteps.cs
@@ -11,56 +11,60 @@ namespace RestSharpProject.Steps
     [Scope(Feature = "User list")]
     public class UserListSteps
     {
-        private IRestClient restClient = new RestClient();
-        private IRestRequest restRequestUserList;
-        private IRestResponse restResponseList = new RestResponse();
-        private string url;
+        private IRestClient _restClient;
+        private IRestRequest _restRequestUserList;
+        private IRestResponse _restResponseList;
+        private string url = "/list";
+
+        public UserListSteps(RestClient restClient)
+        {
+            _restClient = new RestClient(restClient.BaseUrl + url);
+        }
 
         [Given(@"Endpoint is api/list")]
         public void GivenEndpointIsApiList()
         {
-            url = "http://127.0.0.1:8080/api/list";
+            _restClient.BaseUrl = _restClient.BaseUrl;
         }
 
         [When(@"Client sends request to API")]
         public void WhenClientSendsRequestToApi()
         {
-            restRequestUserList = new RestRequest(url);
-            restResponseList = restClient.Get(restRequestUserList);
+            _restRequestUserList = new RestRequest(_restClient.BaseUrl);
+            _restResponseList = _restClient.Get(_restRequestUserList);
         }
 
         [When(@"Client sends request with active parameter set to true")]
         public void WhenClientSendsRequestWithActiveParameterSetToTrue()
         {
-            restRequestUserList = new RestRequest(url + "?active=true");
-            restResponseList = restClient.Get(restRequestUserList);
+            _restRequestUserList = new RestRequest(_restClient.BaseUrl + "?active=true");
+            _restResponseList = _restClient.Get(_restRequestUserList);
         }
 
         [Then(@"Response is successful")]
         public void ThenResponseIsSuccessfull()
         {
-            Assert.That(restResponseList.IsSuccessful, Is.True);
+            Assert.That(_restResponseList.IsSuccessful, Is.True);
         }
 
         [Then(@"contains list of all users")]
         public void ThenContainsListOfAllUsers()
         {
-            List<UserModel> restResponseAllUserList = JsonConvert.DeserializeObject<List<UserModel>>(restResponseList.Content);
+            List<UserModel> restResponseAllUserList = JsonConvert.DeserializeObject<List<UserModel>>(_restResponseList.Content);
             Assert.That(restResponseAllUserList.Count > 0, Is.True);
         }
 
         [Then(@"contains list of active users")]
         public void ThenContainsListOfActiveUsers()
         {
-            List<UserModel> restResponseActiveUserList = JsonConvert.DeserializeObject<List<UserModel>>(restResponseList.Content);
+            List<UserModel> restResponseActiveUserList = JsonConvert.DeserializeObject<List<UserModel>>(_restResponseList.Content);
             if (restResponseActiveUserList.Count == 0)
             {
-                //'[]' is also positive result
                 Assert.IsTrue(true);
             }
             else
             {
-                Assert.That(restResponseList.Content.Contains("\"active\":false"), Is.False);
+                Assert.That(_restResponseList.Content.Contains("\"active\":false"), Is.False);
             }
         }
     }
